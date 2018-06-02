182e82cd2a3ad3d8ce16468d8701ce2a72fa4e31
Curtis Shipley <spurious.thought@gmail.com>
2018-06-02 09:16:35 -0700

    Added clean. Updated iOS Project

---
+++ clean.sh
@@ -0,0 +1,8 @@
+rm -rf ./BlueMonkeyiOS/bin
+rm -rf ./SurveyMonkeyAndroidBindings/bin
+rm -rf ./BlueMonkeyAndroid/bin
+rm -rf ./SurveyMonkeyiOSBindings/bin
+rm -rf ./BlueMonkeyiOS/obj
+rm -rf ./SurveyMonkeyAndroidBindings/obj
+rm -rf ./BlueMonkeyAndroid/obj
+rm -rf ./SurveyMonkeyiOSBindings/obj
